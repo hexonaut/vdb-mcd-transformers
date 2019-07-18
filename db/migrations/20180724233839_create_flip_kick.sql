de508519f7e581309a0b013a21bb5f7ada2896e7
Edvard <edvard@hubinette.me>
2019-07-18 15:29:37 +0200

    Remove _checked constants, use consistens transformer names
    Note this breaks one integration test because vDB core is not updated

--- db/migrations/20180724233839_create_flip_kick.sql
+++ db/migrations/20180724233839_create_flip_kick.sql
@@ -20,7 +20,7 @@ CREATE INDEX flip_kick_header_index
     ON maker.flip_kick (header_id);
 
 ALTER TABLE public.checked_headers
-    ADD COLUMN flip_kick_checked INTEGER NOT NULL DEFAULT 0;
+    ADD COLUMN flip_kick INTEGER NOT NULL DEFAULT 0;
 
 -- +goose Down
 DROP INDEX maker.flip_kick_header_index;
@@ -28,4 +28,4 @@ DROP INDEX maker.flip_kick_header_index;
 DROP TABLE maker.flip_kick;
 
 ALTER TABLE public.checked_headers
-    DROP COLUMN flip_kick_checked;
+    DROP COLUMN flip_kick;
